7b054642b73ec8547326caceed61cd16358a6f0d
Kakon Debnath <kakondebnath96@gmail.com>
2023-12-03 21:25:57 +0600

    fix validation naming convention

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { UserControllers } from './user.controller';
-import { studentValidationSchema } from '../student/student.validation';
+import { createStudentValidationSchema } from '../student/student.validation';
 import requestValidator from '../../middlewares/requestValidator';
 
 const router = Router();
 
 router.post(
   '/create-student',
-  requestValidator(studentValidationSchema),
+  requestValidator(createStudentValidationSchema),
   UserControllers.createStudent,
 );
 
