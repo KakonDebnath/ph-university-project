f2de67e1cdb04a88b645bbc6460d8539078d7889
Kakon Debnath <kakondebnath96@gmail.com>
2023-12-03 19:45:08 +0600

    fix multipul catchAsync function

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,13 +1,8 @@
-import { NextFunction, Request, RequestHandler, Response } from 'express';
+
 import httpStatus from 'http-status';
 import sendResponse from '../../utils/sendResponse';
 import { StudentServices } from './student.service';
-
-const catchAsync = (fn: RequestHandler) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch((err) => next(err));
-  };
-};
+import catchAsync from '../../utils/catchAsync';
 
 const getSingleStudent = catchAsync(async (req, res) => {
   const { studentId } = req.params;
