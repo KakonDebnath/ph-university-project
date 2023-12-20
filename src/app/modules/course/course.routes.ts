c9d84d4f0639d40a87a25b9985c6f616091c56b0
kakon debnath <kakondebnath96@gmail.com>
2023-12-20 13:19:53 +0600

    use auth() for course route

--- src/app/modules/course/course.routes.ts
+++ src/app/modules/course/course.routes.ts
@@ -2,17 +2,26 @@ import { Router } from 'express';
 import { CourseController } from './course.controller';
 import requestValidator from '../../middlewares/requestValidator';
 import { CourseValidations } from './course.validations';
+import auth from '../../middlewares/auth';
 
 const router = Router();
 router.post(
   '/create-course',
+  auth('admin'),
   requestValidator(CourseValidations.createCourseValidationSchema),
   CourseController.createCourse,
 );
 router.get('/', CourseController.getAllCourses);
-router.get('/:id', CourseController.getSingleCourse);
+
+router.get(
+  '/:id',
+  auth('admin', 'faculty', 'student'),
+  CourseController.getSingleCourse,
+);
+
 router.patch(
   '/:id',
+  auth('admin'),
   requestValidator(CourseValidations.updateCourseValidationSchema),
   CourseController.updateCourse,
 );
